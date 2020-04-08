592ff17f19690969802319c3054d809cb4ac1ddf
work <work@ipeitao.com>
2020-04-08 09:42:06 +0800

    278

---
+++ 278. First Bad Version.php
@@ -0,0 +1,33 @@
+<?php
+
+class Solution extends VersionControl
+{
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function firstBadVersion($n)
+    {
+
+        $left = 1;
+        $right = $n;
+
+        while ($left < $right) {
+
+            $mid = $left + ($right - $left) / 2;
+
+            if ($this->isBadVersion($mid)) {
+
+                $right = $mid;
+
+            } else {
+
+                $left = $mid + 1;
+            }
+
+
+        }
+        return floor($left);
+
+    }
+}
